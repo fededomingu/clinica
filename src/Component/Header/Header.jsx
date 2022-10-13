136d30ac20085b9d02c3850cbcd29d4bd0340e89
fededomingu <fededomingu@gmail.com>
2022-10-13 19:48:47 -0300

    Menu responsive - imprtacion de Bootstrap en index.js

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+function Header () {
+
+  const Menu = styled.nav`
+    display: flex;
+    align-items: flex-end;
+    border: 0.5em;
+  `
+  return (
+      
+      <Navbar bg="primary" variant="primary" expand="lg">
+        <Container>
+          <Navbar.Brand as={ Link } to="/">Clinica Odontologica</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Menu>
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={ Link } to="/signin">SingIn</Nav.Link>
+                  <Nav.Link as={ Link } to="/signup">SingUp</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Menu>                  
+        </Container>
+      </Navbar>
+    
+  );
+}
+
+export default Header;
